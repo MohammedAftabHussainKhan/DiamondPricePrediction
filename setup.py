384bea5de5b93eb4945620a2b518e7e3a559e6cd
MohammedAftabHussainKhan <ahkmohammed@gmail.com>
2023-10-05 17:35:02 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages,setup 
+
+
+setup (
+
+    name= "DiamondPricePrewdiction",
+    author="Aftab",
+    author_email='ahkmohammed@gmail.com',
+    version = '0.0.1' ,
+    packages=find_packages(),
+    install_requires=['pandas','numpy','sklearn','seaborn']    
+
+
+
+
+)
